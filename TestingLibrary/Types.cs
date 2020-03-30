cbf518d5aa4b104dc3659b95de4292cd85ed54bc
Kevin Finck <Kevin.Finck@pgn.com>
2020-03-30 09:22:54 -0700

    Update Types.cs

--- TestingLibrary/Types.cs
+++ TestingLibrary/Types.cs
@@ -28,41 +28,65 @@ namespace TestingLibrary
 
     public class SampleProgram
     {
-        class Program
+        private const string SourceOption = "/Source=";
+        //private static readonly string SourceValidValues = Enum<PISources>.AsValidOptionsList;
+
+        //private static Settings Settings = new Settings();
+
+        private static List<PISources> _importSources = null;
+        public List<PISources> ImportSources => _importSources ?? (_importSources = GetSourcesFromArgument());
+
+        private List<PISources> GetSourcesFromArgument()
         {
-            private const string SourceOption = "/Source=";
-            //private static readonly string SourceValidValues = Enum<PISources>.AsValidOptionsList;
+            var sources = new List<PISources>();
 
-            //private static Settings Settings = new Settings();
+            var sourceNames = ""; // CommandLine.ValueOf(SourceOption);
+            if (string.IsNullOrWhiteSpace(sourceNames))
+                return sources;
 
-            private static List<PISources> _importSources = null;
-            public List<PISources> ImportSources => _importSources ?? (_importSources = GetSourcesFromArgument());
+            var parms = sourceNames.Split(',');
 
-            private List<PISources> GetSourcesFromArgument()
+            foreach (var sourceName in parms.Select(s => s?.Trim()?.Trim('"'))) //.Where(s => s.hasValue()))
             {
-                var sources = new List<PISources>();
-
-                var sourceNames = ""; // CommandLine.ValueOf(SourceOption);
-                if (string.IsNullOrWhiteSpace(sourceNames))
-                    return sources;
-
-                var parms = sourceNames.Split(',');
-
-                foreach (var sourceName in parms.Select(s => s?.Trim()?.Trim('"'))) //.Where(s => s.hasValue()))
+                if (!Enum.TryParse(sourceName, true, out PISources source))
                 {
-                    if (!Enum.TryParse(sourceName, true, out PISources source))
-                    {
-                        source = PISources.Unknown;
-                    }
-
-                    if (!sources.Contains(source))
-                        sources.Add(source);
+                    source = PISources.Unknown;
                 }
 
-                return sources;
+                if (!sources.Contains(source))
+                    sources.Add(source);
             }
 
-
+            return sources;
         }
+
+        /*
+        private void Tests()
+        {
+            var val = CommandLine.Arguments;
+            var x = CommandLine.ValueOf(ConfigFileOption);
+            var y = CommandLine.HasArgument(ConfigFileOption);
+
+            var z = Enum<PISources>.AsCsvList;
+            var zz = Enum<PISources>.AsValidOptionsList;
+
+            if (ImportSources.isNullOrEmpty())
+            {
+                isHealthy = false;
+
+                Console.WriteLine();
+                TryMsg(MessageType.Error,
+                    $"A \"/Source=\" parameter must be supplied using a valid option: {SourceValidValues}. {(CommandLine.HasArgument(ConfigFileOption) ? $"Parameter value \"{CommandLine.ValueOf(ConfigFileOption)}\" is invalid." : "")}");
+            }
+            else if (ImportSources.Contains(PISources.Unknown))
+            {
+                isHealthy = false;
+
+                Console.WriteLine();
+                TryMsg(MessageType.Error,
+                    $"One or more values listed in the \"/Source=\" parameter is invalid.{(CommandLine.HasArgument(ConfigFileOption) ? $" Parameter value: \"{CommandLine.ValueOf(ConfigFileOption)}\" " : " ")}Valid options are: {SourceValidValues}.");
+            }
+        }
+        */
     }
 }
